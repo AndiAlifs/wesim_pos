0f4c5644ff49b2a6c66e069e4481424f68fa1a4f
Andi Alifsyah <andyalyfsyah4@gmail.com>
2021-02-26 13:08:42 +0800

    templating adminLTE

---
+++ resources/views/adminlte/partials/sidebar.blade.php
@@ -0,0 +1,30 @@
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+  <!-- Brand Logo -->
+  <a href="../../index3.html" class="brand-link">
+    <img src="{{asset('/adminlte/dist/img/67Dev.jpg')}}"
+        alt="67 Logo"
+        class="brand-image img-circle elevation-3"
+        style="opacity: .8">
+    <span class="brand-text font-weight-light">Kelompok 67</span>
+  </a>
+
+  <!-- Sidebar -->
+  <div class="sidebar">
+    <!-- Sidebar Menu -->
+    <nav class="mt-2">
+      <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+        <li class="nav-header">MAIN MENU</li>
+        <li class="nav-item">
+          <a href="/questions" class="nav-link">
+            <i class="nav-icon fas fa-table"></i>
+            <p>
+              Stackoverflow
+            </p>
+          </a>
+        </li>
+      </ul>
+    </nav>
+    <!-- /.sidebar-menu -->
+  </div>
+  <!-- /.sidebar -->
+</aside>
