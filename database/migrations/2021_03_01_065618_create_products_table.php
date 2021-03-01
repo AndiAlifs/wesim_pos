26d5c8c95885ef658a396349a25c45388d8b853a
Andi Alifsyah <andyalyfsyah4@gmail.com>
2021-03-01 15:19:40 +0800

    table baru

--- database/migrations/2021_03_01_065618_create_products_table.php
+++ database/migrations/2021_03_01_065618_create_products_table.php
@@ -15,6 +15,11 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('code');            
+            $table->string('name');            
+            $table->string('produsen');
+            $table->integer('purchase_price');
+            $table->string('description');              
             $table->timestamps();
         });
     }
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -14,15 +14,14 @@ class UsersTableSeeder extends Seeder
         $data = [];
         $faker = Faker\Factory::create();
 
-        for($i=0;$i<15;$i++){
-        $data[$i] = [
-                'name' => $faker->name,
-                'email' => $faker->unique()->safeEmail,
-                'username' => $faker->unique()->userName,
-                'role_id' => rand(1,3),
-                'password' => bcrypt('12345'),
-                'remember_token' => Str::random(10),
-            ];
+        for($i=0;$i<5;$i++){
+            $data[$i] = [
+                    'name' => $faker->name,
+                    'email' => $faker->unique()->safeEmail,
+                    'username' => $faker->unique()->userName,
+                    'role_id' => rand(1,3),
+                    'password' => bcrypt('12345'),
+                ];
         }
         DB::table('users')->insert($data);
     }
