9138bd606557ca03e8000c7bcd7f7d78af6ddab2
Andi Alifsyah <andyalyfsyah4@gmail.com>
2021-03-02 14:59:34 +0800

    add one to many relation

--- app/Role.php
+++ app/Role.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
-    //
+    protected $guarded = [];
+
+    public function users()
+    {
+        return $this->hasMany('App\User');
+    }
 }
--- app/User.php
+++ app/User.php
@@ -6,7 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable 
 {
     use Notifiable;
 
@@ -34,4 +34,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function role()
+    {
+        return $this->belongsTo('App\Role',);
+    }
 }
