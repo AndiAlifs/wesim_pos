1e610e24438ec24277dc118b2d6c2e0c1f12328b
elkodedy <elkodedy.99@gmail.com>
2021-03-04 16:21:39 +0800

    display user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\user;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
@@ -15,8 +14,8 @@ class UserController extends Controller
      */
     public function index()
     {
-    	$user = DB::table('users')->get();
-    	return view('adminlte/user/user',['user' => $user]);
+    	$users = User::with("role")->get();
+    	return view('adminlte/user/user',['user' => $users]);
     }
 
     /**
