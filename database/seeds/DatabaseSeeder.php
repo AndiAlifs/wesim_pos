64d1a8df222455389a0487850d3429434ba06bf6
Andi Alifsyah <andyalyfsyah4@gmail.com>
2021-03-04 16:29:28 +0800

    product seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(RoleSeeder::class);
         $this->call(UsersTableSeeder::class);
+        $this->call(CategorySeeder::class);
+        $this->call(ProductSeeder::class);
     }
 }
--- database/seeds/ProductSeeder.php
+++ database/seeds/ProductSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Product;
 
 class ProductSeeder extends Seeder
 {
@@ -11,6 +12,24 @@ class ProductSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $faker = Faker\Factory::create();
+
+        $purchase_price = [20000,10000,15000];
+        $price = [28000,15000,20000];
+
+        for ($i=0; $i <= 10; $i++) { 
+            $new_product = new Product;
+
+            $new_product->name = $faker->firstName();
+            $new_product->product_code = $faker->isbn10();
+            $new_product->producer = $faker->lastName();
+            $new_product->description = $faker->sentence(5);
+            $temp_angka = rand(0,2);
+            $new_product->purchase_price = $purchase_price[$temp_angka];
+            $new_product->price = $price[$temp_angka];
+            $new_product->save();
+
+            $new_product->Category()->attach(rand(1,3));
+        }
     }
 }
