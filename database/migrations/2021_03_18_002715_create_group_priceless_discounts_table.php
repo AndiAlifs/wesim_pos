d2464648363f6240453f74aac0b5284dbb8cc311
fatwaanugerah21 <fatwaanugerah21@gmail.com>
2021-03-18 08:33:10 +0800

    adding discount database

--- database/migrations/2021_03_18_002715_create_group_priceless_discounts_table.php
+++ database/migrations/2021_03_18_002715_create_group_priceless_discounts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateDiscountsTable extends Migration
+class CreateGroupPricelessDiscountsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,19 @@ class CreateDiscountsTable extends Migration
      */
     public function up()
     {
-        Schema::create('discounts', function (Blueprint $table) {
+        Schema::create('group_priceless_discounts', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->integer('discountAmount');
-            $table->string('description')->default('');
+
             $table->unsignedBigInteger('product_id');
             $table->foreign('product_id')->references('id')->on('products');
             
+            $table->date('effective_date');
+            $table->date('end_date');
+            $table->integer('discount_amount');
+            $table->string('description')->default('');
+            
             $table->timestamps();
+
         });
     }
 
@@ -31,6 +36,6 @@ class CreateDiscountsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('discounts');
+        Schema::dropIfExists('group_priceless_discounts');
     }
 }
---
+++ database/migrations/2021_03_18_002814_create_product_discounts_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductDiscountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_discounts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->unsignedBigInteger('product_id');
+            $table->foreign('product_id')->references('id')->on('products');
+
+            $table->date('effective_date');
+            $table->date('end_date');
+            $table->integer('discount_amount');
+            $table->string('description')->default('');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_discounts');
+    }
+}
