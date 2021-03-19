b0dc8a12adb7657307463ce93325170f67e543f6
Andi Alifsyah <andyalyfsyah4@gmail.com>
2021-03-19 17:36:26 +0800

    index visitor export

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -3,11 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Member;
+use App\Exports\VisitorExport;
+use Maatwebsite\Excel\Facades\Excel;
 
 class ReportController extends Controller
 {
     public function index()
+    {   
+        $jumlahMember = Member::get()->count();
+        // dd($jumlahMember);
+        return view('adminlte/report/report',compact('jumlahMember'));
+    }
+
+    public function indexVisitor()
     {
-        return view('adminlte/report/report');
+        return Excel::download(new VisitorExport, 'visitors.xlsx');    
     }
 }
