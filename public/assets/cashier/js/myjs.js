b5af616fac5edeb5b5b66e19ed475d6ebd52f50a
elkodedy <elkodedy.99@gmail.com>
2021-03-19 15:16:14 +0800

    buat tab hold

--- public/assets/cashier/js/myjs.js
+++ public/assets/cashier/js/myjs.js
@@ -20,29 +20,56 @@ $(".search-box").on("keyup", function (e) {
     }
 });
 
-// tab clik
+var selling_transaction_id = $('.trx-active > .trx > input').val();
+// function activateTab(){}
 $('.trx-tab').on('click', function () {
+    console.log('activateTab()');
+    localStorage.tab_index = $(this).closest('.tab-parent').index();
+
+    // toogle activate
     $('.trx-active').removeClass('trx-active');
     $(this).addClass('trx-active');
+
+    // .trx-active set width
+    $('#minwidth-18').attr('id', '');
+    $(this).closest('.tab-parent').attr('id', 'minwidth-18');
+
+    // get this tab selling_transaction_id
+    selling_transaction_id = $('.trx-active > .trx > input').val();
+    loadCart();
+    $(".search-box")[0].focus();
 })
 
+// auto load when reload
+tabEmpty(); //automatic cek if tab is empty
+$('.trx-tab')[localStorage.tab_index].classList.add('trx-active'); // add active to active tab
+$('.trx-tab')[localStorage.tab_index].parentNode.id = "minwidth-18"; // set width to active tab
+loadCart(); //automatic load cart
 
-// auto load first transaction
-loadCart();
-$('.trx-tab')[localStorage.tab_index].classList.add('trx-active');
+function allFunction() { //note
+    // general
+    tabEmpty(); // if tab is emprty > make a new transaction
+    addNewTransaction(); // add a new transaction "tombol '+' di tab, tombol hold"
+    filterCategory(); // filter by product category
+    searchBox(); // search box
+    callModal(); // call product modal
+    addToCart(); // add to this transaction cart "tombol tambahkan ke keranjang"
+    loadCart(); // load cart of this transaction
+    createList(); // load loop
+    deleteBtn(); // delete this item in cart "tombol delete item di cart"
+    deleteCart(); // delete this transaction "tombol batal"
+}
 
-// set new transcation
-function setSellingTransactionId(id, index) {
-    localStorage.selling_transaction_id = id;
-    localStorage.tab_index = (index - 1);
-    $('.trx-active').removeClass('trx-active');
-    $(this).addClass('trx-active');
-
-    loadCart();
+// if tab is emprty > make a new transaction
+function tabEmpty() {
+    if ($('.tab-parent').html() == null)
+        addNewTransaction();
 }
 
 // new transaction
 function addNewTransaction() {
+    console.log('addNewTransaction()');
+    localStorage.tab_index = $('.tab-parent').length;
     var user_id = $('#user-id').val();
     $.ajax({
         type: "POST",
@@ -52,39 +79,12 @@ function addNewTransaction() {
             '_token': $('input[name=_token]').val(),
         },
         success: function (data) {
-            localStorage.selling_transaction_id = data;
-            localStorage.tab_index = $('.tab-parent').length;
             location.reload();
         }
     });
 }
 
-function deleteCart() {
-    selling_transaction_id = localStorage.selling_transaction_id;
-
-    //cek if cart not empty
-    // if ($('.cart-list').html() != "")
-    var confirm_delete = confirm('anda yakin menghapus keranjang ini ??')
-    if (!confirm_delete)
-        return 0;
-
-    $.ajax({
-        type: "GET",
-        url: "/cashier/delete_cart",
-        data: {
-            'selling_transaction_id': selling_transaction_id,
-            '_token': $('input[name=_token]').val(),
-        },
-        success: function (data) {
-            console.log('susces');
-            if (localStorage.tab_index == ($('.tab-parent').length - 1))
-                localStorage.tab_index = (localStorage.tab_index - 1);
-            location.reload();
-        }
-    });
-}
-
-// menu
+// filter by category
 function filterCategory(category_id) {
     $.ajax({
         type: "POST",
@@ -100,7 +100,7 @@ function filterCategory(category_id) {
     });
 }
 
-function search_box(key) {
+function searchBox(key) {
     $.ajax({
         type: "POST",
         url: "/cashier/search_box",
@@ -116,8 +116,68 @@ function search_box(key) {
     });
 }
 
+// call mmodal when product clicked
+function callModal(selling_id, product_id) {
+
+    $("#modal-id").val(selling_id);
+    $("#modal-product-id").val(product_id);
+    selling_transaction_id = $('.trx-active > .trx > input').val();
+
+    $.ajax({
+        type: "POST",
+        url: "/cashier/get_modal_data",
+        data: {
+            'selling_id': selling_id,
+            'product_id': product_id,
+            'selling_transaction_id': selling_transaction_id,
+            '_token': $('input[name=_token]').val(),
+        },
+        success: function (data) {
+            if (data.already_in_cart) {
+                $("#modal-name").html(data.product['name']);
+                $("#modal-price").val(data.product['price']);
+                $("#modal-amount").val(data['amount']);
+            } else {
+                $("#modal-name").html(data['name']);
+                $("#modal-price").val(data['price']);
+                $("#modal-amount").val(1);
+            }
+            // $("#modal-btn").attr("onclick", ("addToCart('" + $("#amount").val() + "')"));
+        }
+    });
+}
+
+// add to cart from modal data
+function addToCart() {
+    // get data
+    var selling_transaction_id = $('.trx-active > .trx > input').val();
+    var product_id = $("#modal-product-id").val();
+    var product_amount = $("#modal-amount").val();
+
+    $.ajax({
+        type: "POST",
+        url: "/cashier/add_to_cart",
+        data: {
+            'selling_transaction_id': selling_transaction_id,
+            'product_id': product_id,
+            'product_amount': product_amount,
+            '_token': $('input[name=_token]').val(),
+        },
+        success: function (data) {
+            // console.log(data);
+        }
+    })
+    // reset when close modal close
+    loadCart();
+    $(".search-box")[0].focus();
+
+    // window.scrollTo(0,$('#scrolltohere').scrollHeight);
+}
+
+// load cart
 function loadCart() {
-    var selling_transaction_id = localStorage.selling_transaction_id;
+    console.log('loadCart()');
+    selling_transaction_id = $('.trx-active > .trx > input').val();
     $(".cart-list")[0].innerHTML = "";
     $(".total-price")[0].innerHTML = "0";
     total_price = 0;
@@ -157,65 +217,9 @@ function createList(selling, index) {
     $(".total-price")[0].innerHTML = new Intl.NumberFormat("id-ID").format(total_price);
 }
 
-function callModal(selling_id, product_id) {
-
-    $("#modal-id").val(selling_id);
-    $("#modal-product-id").val(product_id);
-    selling_transaction_id = localStorage.selling_transaction_id;
-
-    $.ajax({
-        type: "POST",
-        url: "/cashier/get_modal_data",
-        data: {
-            'selling_id': selling_id,
-            'product_id': product_id,
-            'selling_transaction_id': selling_transaction_id,
-            '_token': $('input[name=_token]').val(),
-        },
-        success: function (data) {
-            if (data.already_in_cart) {
-                $("#modal-name").html(data.product['name']);
-                $("#modal-price").val(data.product['price']);
-                $("#modal-amount").val(data['amount']);
-            } else {
-                $("#modal-name").html(data['name']);
-                $("#modal-price").val(data['price']);
-                $("#modal-amount").val(1);
-            }
-            // $("#modal-btn").attr("onclick", ("addToCart('" + $("#amount").val() + "')"));
-        }
-    });
-}
-
-function addToCart() {
-    // get data
-    var selling_transaction_id = localStorage.selling_transaction_id;
-    var product_id = $("#modal-product-id").val();
-    var product_amount = $("#modal-amount").val();
-
-    $.ajax({
-        type: "POST",
-        url: "/cashier/add_to_cart",
-        data: {
-            'selling_transaction_id': selling_transaction_id,
-            'product_id': product_id,
-            'product_amount': product_amount,
-            '_token': $('input[name=_token]').val(),
-        },
-        success: function (data) {
-            // console.log(data);
-        }
-    })
-    loadCart();
-
-    // reset when close modal close
-    $(".search-box")[0].focus();
-
-    // window.scrollTo(0,$('#scrolltohere').scrollHeight);
-}
-
-// ----------- selesai
+// 'x' button in cart
 function deleteBtn(selling_id) {
+    console.log('deleteBtn()');
     $.ajax({
         type: "POST",
         url: "/cashier/delete_item",
@@ -228,4 +232,55 @@ function deleteBtn(selling_id) {
         }
     })
     loadCart();
+}
+// batal button
+function deleteCart() {
+    // if delete the last cart
+    if (localStorage.tab_index == ($('.tab-parent').length - 1))
+        localStorage.tab_index = localStorage.tab_index - 1;
+
+    selling_transaction_id = $('.trx-active > .trx > input').val();
+
+    //cek if cart not empty
+    // if ($('.cart-list').html() != "")
+    var confirm_delete = confirm('anda yakin menghapus keranjang ini ??')
+    if (!confirm_delete)
+        return 0;
+
+    $.ajax({
+        type: "GET",
+        url: "/cashier/delete_cart",
+        data: {
+            'selling_transaction_id': selling_transaction_id,
+            '_token': $('input[name=_token]').val(),
+        },
+        success: function (data) {
+            console.log('susces');
+            location.reload();
+        }
+    });
+}
+
+// close tab
+function closeTab(selling_transaction_id) {
+    // if close the last cart
+    if (localStorage.tab_index == ($('.tab-parent').length - 1))
+        localStorage.tab_index = localStorage.tab_index - 1;
+
+    var confirm_delete = confirm('anda yakin menghapus keranjang ini ??')
+    if (!confirm_delete)
+        return 0;
+
+    $.ajax({
+        type: "GET",
+        url: "/cashier/delete_cart",
+        data: {
+            'selling_transaction_id': selling_transaction_id,
+            '_token': $('input[name=_token]').val(),
+        },
+        success: function (data) {
+            console.log('susces');
+            location.reload();
+        }
+    });
 }
