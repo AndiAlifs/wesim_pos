e4dfca7ebec77ca4c92eeafbf08f002a3b195c98
Andi Alifsyah <andyalyfsyah4@gmail.com>
2021-03-19 17:36:36 +0800

    redirect to report

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,7 +23,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('adminlte/report/report');    
+        return redirect()->route('report');    
     }
 
     public function cashier()
