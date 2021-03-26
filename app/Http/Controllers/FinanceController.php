94caf7048faaf87a1ae81f0655e15ee65396c659
Andi Alifsyah <andyalyfsyah4@gmail.com>
2021-03-26 14:59:25 +0800

    finance query between month

--- app/Http/Controllers/FinanceController.php
+++ app/Http/Controllers/FinanceController.php
@@ -15,28 +15,44 @@ class FinanceController extends Controller
      */
     public function index()
     {   
+        $bulan_name = [".","Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"];
+        
         if($_GET){
-            $waktu["bulan"] = sprintf("%02d", $_GET["bulan"]);
-            $waktu["tahun"] = $_GET["tahun"];
+            $waktu["bulan_start"] = sprintf("%02d", $_GET["bulan_start"]);
+            $waktu["bulan_name_start"] = $bulan_name[$_GET["bulan_start"]];
+            $waktu["tahun_start"] = $_GET["tahun_start"];
+            $waktu["bulan_end"] = sprintf("%02d", $_GET["bulan_end"]);
+            $waktu["bulan_name_end"] = $bulan_name[$_GET["bulan_end"]];
+            $waktu["tahun_end"] = $_GET["tahun_end"];
         } else {
-            $waktu["bulan"] = sprintf("%02d",Carbon::now()->month);
-            $waktu["tahun"] = Carbon::now()->year;
+            $waktu["bulan_start"] = sprintf("%02d",Carbon::now()->month);
+            $waktu["bulan_name_start"] = $bulan_name[Carbon::now()->month];
+            $waktu["tahun_start"] = Carbon::now()->year;
+            $waktu["bulan_end"] = sprintf("%02d",Carbon::now()->month);
+            $waktu["bulan_name_end"] = $bulan_name[Carbon::now()->month];
+            $waktu["tahun_end"] = Carbon::now()->year;
         }
 
-        $dateFrom = $waktu["tahun"]."-".$waktu["bulan"]."-01";     
-        $dateTo = $waktu["tahun"]."-".$waktu["bulan"]."-31";     
+        $dateFrom = $waktu["tahun_start"]."-".$waktu["bulan_start"]."-01";     
+        $dateTo = $waktu["tahun_end"]."-".$waktu["bulan_end"]."-31";     
 
         // Query between
         $finances = finance::whereBetween('transaction_date',[$dateFrom, $dateTo])
-                                ->orderByDesc('transaction_date')
+                                ->orderBy('transaction_date')
                                 ->get();
-        // dd($finances);
 
-        $kas["debit"] = finance::where('jenis','debit')->sum('amount');
-        $kas["kredit"] = finance::where('jenis','kredit')->sum('amount');
+        $kas["debit"] = 0;
+        $kas["kredit"] = 0;
+        foreach ($finances as $finance ) {
+            if ($finance->jenis == "debit"){
+                $kas["debit"] += $finance["amount"];
+            } else {
+                $kas["kredit"] += $finance["amount"];
+            }
+        }
 
-        // dd($kas);
-        return view('adminlte/finance/finance', compact('finances','kas'));
+
+        return view('adminlte/finance/finance', compact('finances','kas','waktu'));
     }
 
     /**
--- database/seeds/FinanceSeeder.php
+++ database/seeds/FinanceSeeder.php
@@ -19,49 +19,98 @@ class FinanceSeeder extends Seeder
             "transaction_name" => "Kas",
             "amount" => 1050000,
             "jenis" => "debit",
-            "transaction_date" => $dateNow->toDateString()
+            "transaction_date" => '2021-03-01'
         ]);
         finance::create([
             "transaction_name" => "ATK",
             "amount" => 200000,
             "jenis" => "debit",
-            "transaction_date" => $dateNow->yesterday()->toDateString()
+            "transaction_date" => '2021-03-02'
         ]);
         finance::create([
             "transaction_name" => "Bahan Habis Pakai",
             "amount" => 250000,
             "jenis" => "debit",
-            "transaction_date" => $dateNow->yesterday()->toDateString()
-        ]);
-        finance::create([
-            "transaction_name" => "Pendapatan",
-            "amount" => 2000000,
-            "jenis" => "kredit",
-            "transaction_date" => $dateNow->toDateString()
-        ]);
+            "transaction_date" => '2021-03-15'
+        ]); 
         finance::create([
             "transaction_name" => "Transportasi",
             "amount" => 800000,
             "jenis" => "debit",
-            "transaction_date" => $dateNow->yesterday()->toDateString()
+            "transaction_date" => '2021-03-18'
         ]);
         finance::create([
             "transaction_name" => "Gaji",
             "amount" => 2500000,
             "jenis" => "debit",
-            "transaction_date" => $dateNow->yesterday()->toDateString()
+            "transaction_date" => '2021-03-19'
         ]);
         finance::create([
             "transaction_name" => "Utang Bank",
             "amount" => 1500000,
             "jenis" => "kredit",
-            "transaction_date" => $dateNow->yesterday()->yesterday()->toDateString()
+            "transaction_date" => '2021-03-22'
         ]);
         finance::create([
             "transaction_name" => "Modal Alip",
             "amount" => 1300000,
             "jenis" => "kredit",
-            "transaction_date" => $dateNow->toDateString()
+            "transaction_date" => '2021-03-25'
+        ]);
+        finance::create([
+            "transaction_name" => "Pendapatan",
+            "amount" => 2000000,
+            "jenis" => "kredit",
+            "transaction_date" => '2021-03-31'
+        ]);
+
+        finance::create([
+            "transaction_name" => "Kas",
+            "amount" => 1550000,
+            "jenis" => "debit",
+            "transaction_date" => '2021-04-01'
+        ]);
+        finance::create([
+            "transaction_name" => "ATK",
+            "amount" => 200000,
+            "jenis" => "debit",
+            "transaction_date" => '2021-04-02'
+        ]);
+        finance::create([
+            "transaction_name" => "Bahan Habis Pakai",
+            "amount" => 250000,
+            "jenis" => "debit",
+            "transaction_date" => '2021-04-15'
+        ]); 
+        finance::create([
+            "transaction_name" => "Transportasi",
+            "amount" => 800000,
+            "jenis" => "debit",
+            "transaction_date" => '2021-04-18'
+        ]);
+        finance::create([
+            "transaction_name" => "Gaji",
+            "amount" => 2500000,
+            "jenis" => "debit",
+            "transaction_date" => '2021-04-19'
+        ]);
+        finance::create([
+            "transaction_name" => "Utang Bank",
+            "amount" => 1700000,
+            "jenis" => "kredit",
+            "transaction_date" => '2021-04-22'
+        ]);
+        finance::create([
+            "transaction_name" => "Modal Alip",
+            "amount" => 1500000,
+            "jenis" => "kredit",
+            "transaction_date" => '2021-04-25'
+        ]);
+        finance::create([
+            "transaction_name" => "Pendapatan",
+            "amount" => 2100000,
+            "jenis" => "kredit",
+            "transaction_date" => '2021-04-30'
         ]);
     }
 }
