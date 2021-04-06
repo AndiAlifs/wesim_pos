8f008905119f76acd6c1ae2719b113e020ad5141
Andi Alifsyah <andyalyfsyah4@gmail.com>
2021-04-06 18:14:38 +0800

    selling seeder 14 days

--- database/seeds/SellingSeeder.php
+++ database/seeds/SellingSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use App\Selling;
+use App\SellingTransaction;
 
 class SellingSeeder extends Seeder
 {
@@ -11,21 +12,23 @@ class SellingSeeder extends Seeder
      * @return void
      */
     public function run()
-    {
-        $transaction_number_done = [1, 2, 3, 4, 5];
-        for ($i = 0; $i <= rand(50, 100); $i++) {
-            Selling::create([
-                "selling_transaction_id" => $transaction_number_done[rand(0, 4)],
-                "product_id" => rand(1, 50),
-                "date" => Carbon\Carbon::now()->toDateString(),
-                "amount" => rand(1, 10),
-                "price" => 0,
-            ]);
+    {   
+        $jumlahTransaksi = SellingTransaction::count();
+        for ($j=1; $j <= $jumlahTransaksi-1; $j++) { 
+            for ($i = 0; $i <= rand(20, 20); $i++) {
+                Selling::create([
+                    "selling_transaction_id" => $j,
+                    "product_id" => rand(1, 50),
+                    "date" => Carbon\Carbon::now()->toDateString(),
+                    "amount" => rand(1, 10),
+                    "price" => 0,
+                ]);
+            }
         }
-        $transaction_number_holded = [6];
+
         for ($i = 0; $i <= rand(5, 10); $i++) {
             Selling::create([
-                "selling_transaction_id" => $transaction_number_holded[rand(0, 0)],
+                "selling_transaction_id" => $jumlahTransaksi,
                 "product_id" => rand(1, 50),
                 "date" => Carbon\Carbon::now()->yesterday()->toDateString(),
                 "amount" => rand(1, 10),
