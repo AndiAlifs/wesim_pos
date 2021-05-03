6c6abd78081101b2a02799db5c5fb38853ac2387
Andi Alifsyah <andyalyfsyah4@gmail.com>
2021-05-03 15:27:01 +0800

    responsive inventory

--- resources/views/adminlte/inventory/inventory.blade.php
+++ resources/views/adminlte/inventory/inventory.blade.php
@@ -23,7 +23,7 @@
                         @endif
                         <div id="example1_wrapper" class="dataTables_wrapper dt-bootstrap4">
                             <div class="row">
-                                <div class="col-sm-12">
+                                <div class="col-sm-12 table-responsive">
                                     <table id="datatable_pagination"
                                         class="table table-bordered table-striped dataTable dtr-inline" role="grid"
                                         aria-describedby="example1_info">
